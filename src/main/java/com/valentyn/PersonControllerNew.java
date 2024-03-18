28a9becf80fe03d2412127b75e5f13acc2804966
valentynliubchenko <luybchenko.val@gmail.com>
2024-03-18 22:20:53 +0200

    add createPersonNew

--- src/main/java/com/valentyn/PersonControllerNew.java
+++ src/main/java/com/valentyn/PersonControllerNew.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PersonControllerNew {
@@ -20,5 +22,13 @@ public class PersonControllerNew {
     public ResponseEntity<Person> createPerson() {
         return new ResponseEntity<>(personInterface.createPerson(), HttpStatusCode.valueOf(200));
     }
+
+    @GetMapping("/createPersonNew")
+    @ResponseBody
+    public Person createPersonNew() {
+        return personInterface.createPerson();
+    }
+
+
 
 }
